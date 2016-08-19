08ec6572b6dadb6999ce32a7c4be778924408964
shahz <shahz@DESKTOP-A4RB8C0>
2016-08-19 11:07:50 -0400

    added an image to the splash form

--- COMP123-S2016-FinalExam/SplashForm.Designer.cs
+++ COMP123-S2016-FinalExam/SplashForm.Designer.cs
@@ -42,8 +42,12 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.BackgroundImage = global::COMP123_S2016_FinalExam.Properties.Resources.splashSceen;
+            this.ClientSize = new System.Drawing.Size(600, 320);
+            this.ControlBox = false;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "SplashForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SplashForm";
